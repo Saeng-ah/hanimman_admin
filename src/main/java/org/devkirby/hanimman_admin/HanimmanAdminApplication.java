ead5ac99d9ad3450dab13fd72ab511599ace7770
nechya00 <122517224+Saeng-ah@users.noreply.github.com>
2024-11-28 10:44:30 +0900

    Initial commit

---
+++ src/main/java/org/devkirby/hanimman_admin/HanimmanAdminApplication.java
@@ -0,0 +1,13 @@
+package org.devkirby.hanimman_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanimmanAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanimmanAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devkirby/hanimman_admin/HanimmanAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devkirby.hanimman_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanimmanAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
